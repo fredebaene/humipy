ab2d3774dc0d0e930d3bb5dc98beeda9cf361e9d
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-16 12:37:50 +0100

    Update

--- src/humipy/meteomatics.py
+++ src/humipy/meteomatics.py
@@ -12,9 +12,13 @@ import os
 import requests
 from typing import Union
 from dotenv import load_dotenv
+from datetime import datetime
+from dateutil import tz
 
 
-_METEOMATICS_BASE_URL="https://api.meteomatics.com/2023-12-16T00:00:00Z/t_2m:C/52.520551,13.461804/html"
+_LOCATION = "52.520551,13.461804"
+_METEOMATICS_BASE_URL="https://api.meteomatics.com"
+_METEOMATICS_URL_SUFFIX = "t_2m:C/52.520551,13.461804/json"
 _METEOMATICS_TOKEN_URL="https://login.meteomatics.com/api/v1/token"
 
 
@@ -36,12 +40,20 @@ def _get_access_token(token_url: str) -> Union[str, None]:
         return response.json().get("access_token")
     return None
 
+
+def _construct_url(dt: datetime, weather_params: list, location: str) -> str:
+    dt_str = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    url = f"{_METEOMATICS_BASE_URL}/{dt_str}/{','.join(weather_params)}/{location}/json"
+    return url
+
+
 def main() -> None:
     load_dotenv()
     access_token = _get_access_token(_METEOMATICS_TOKEN_URL)
-    response = requests.get(
-        _METEOMATICS_BASE_URL, {"access_token": access_token})
-    print(response)
+    weather_params = ["t_2m:C"]
+    url = _construct_url(datetime.now(tz=tz.UTC), weather_params, _LOCATION)
+    response = requests.get(url, {"access_token": access_token})
+    print(response.text)
 
 
 if __name__ == "__main__":
