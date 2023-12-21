cf22599f7309d4702a4caf9c3f9dab8e8068c1a5
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-21 16:46:05 +0100

    Define get_engine() function:

--- src/humipy/send.py
+++ src/humipy/send.py
@@ -12,22 +12,34 @@ import pandas as pd
 from typing import Tuple
 
 
-def _construct_locations_table(metadata: MetaData) -> Tuple[MetaData, Table]:
-    """
-    This function initializes the locations table and add it to the metadata 
-    object.
+def _get_engine():
+    load_dotenv()
+    db_name = os.getenv("_HUMIPY_DB_NAME")
+    db_port = os.getenv("_HUMIPY_DB_PORT")
+    db_username = os.getenv("_HUMIPY_DB_USERNAME")
+    db_password = os.getenv("_HUMIPY_DB_PASSWORD")
 
-    Args:
-        metadata (MetaData): metadata object to which to add the table.
+    engine = create_engine(
+        f"postgresql+psycopg2://{db_username}:{db_password}@localhost:{db_port}/{db_name}"
+    )
+
+    return engine
+
+
+def _construct_locations_table() -> Tuple[MetaData, Table]:
+    """
+    This function initializes the locations table and adds it to the metadata 
+    object.
 
     Returns:
         Tuple[MetaData, Table]: the metadata object and the locations table.
     """
+    metadata = MetaData()
     locations = Table(
         "locations",
         metadata,
-        Column("id", Integer, primary_key=True),
-        Column("name", String),
+        Column("location_id", Integer, primary_key=True),
+        Column("location_name", String),
     )
     return (metadata, locations)
 
