670eba33bdca2dd83ee51f3090a822b00cb9da41
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-21 16:46:27 +0100

    Adjust functions

--- src/humipy/send.py
+++ src/humipy/send.py
@@ -44,43 +44,17 @@ def _construct_locations_table() -> Tuple[MetaData, Table]:
     return (metadata, locations)
 
 
-def _get_locations(
-        conn: sqlalchemy.engine.base.Connection,
-        locations: Table) -> pd.DataFrame:
+def get_locations() -> pd.DataFrame:
     """
     This function gets all the sensor locations and returns them in a data 
     frame.
 
-    Args:
-        conn (sqlalchemy.engine.base.Connection): the connection to the 
-            database.
-        locations (Table): the locations table.
-
     Returns:
         pd.DataFrame: a data frame with all sensor locations.
     """
+    engine = _get_engine()
+    metadata, locations = _construct_locations_table()
     stmt = select(locations)
-    return pd.read_sql_query(sql=stmt, con=conn)
-
-
-def main() -> None:
-    load_dotenv()
-    db_name = os.getenv("_HUMIPY_DB_NAME")
-    db_port = os.getenv("_HUMIPY_DB_PORT")
-    db_username = os.getenv("_HUMIPY_DB_USERNAME")
-    db_password = os.getenv("_HUMIPY_DB_PASSWORD")
-
-    engine = create_engine(
-        f"postgresql+psycopg2://{db_username}:{db_password}@localhost:{db_port}/{db_name}"
-    )
-
-    metadata = MetaData()
-    metadata, locations = _construct_locations_table(metadata)
-
     with engine.connect() as conn:
-        locations = _get_locations(conn, locations)
-
-
-
-if __name__ == "__main__":
-    main()
+        locations = pd.read_sql_query(sql=stmt, con=conn)
+    return locations
