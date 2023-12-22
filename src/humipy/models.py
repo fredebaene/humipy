ea70ac91c059a005b1b770a94d03b1ab8b3b8b39
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-23 00:32:32 +0100

    Define locations table

--- src/humipy/models.py
+++ src/humipy/models.py
@@ -1,5 +1,16 @@
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped
+from sqlalchemy.orm import mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class Location(Base):
+    __tablename__ = "locations"
+
+    location_id: Mapped[int] = mapped_column(primary_key=True)
+    location_name: Mapped[str] = mapped_column()
+
+    def __repr__(self):
+        return f"Location(ID: {self.location_id}, Name: {self.location_name})"
