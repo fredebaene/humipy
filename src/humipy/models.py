ed3d604f0525658e29fcbfc373667af8a0ab6681
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-23 00:35:12 +0100

    Define sensors table

--- src/humipy/models.py
+++ src/humipy/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.orm import mapped_column
+from typing import Optional
 
 
 class Base(DeclarativeBase):
@@ -14,3 +15,14 @@ class Location(Base):
 
     def __repr__(self):
         return f"Location(ID: {self.location_id}, Name: {self.location_name})"
+    
+
+class Sensor(Base):
+    __tablename__ = "sensors"
+
+    sensor_id: Mapped[int] = mapped_column(primary_key=True)
+    sensor_name: Mapped[str] = mapped_column()
+    sensor_serial_number: Mapped[Optional[int]] = mapped_column()
+
+    def __repr__(self):
+        return f"Sensor(ID: {self.sensor_id}, Name: {self.sensor_name}, Serial nr.: {self.sensor_serial_number})"
