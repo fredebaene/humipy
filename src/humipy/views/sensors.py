c9924ace8d5dfe2fed9616e3c02098fd0837902d
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-24 10:21:30 +0100

    Add function to add sensor

--- src/humipy/views/sensors.py
+++ src/humipy/views/sensors.py
@@ -33,3 +33,37 @@ def render_sensors_table(engine: sqlalchemy.engine.base.Engine) -> str:
     console.print(Panel("Available Sensors"))
     console.print(Padding(table, (0, 0, 0, 2)))
     return "d"
+
+
+def render_sensor_addition(engine: sqlalchemy.engine.base.Engine) -> str:
+    """
+    This function renders a prompt which asks the user for a new sensor. The 
+    prompt for a new sensor is repeated until the user enters a new 
+    sensor, i.e., a sensor that is not already in the database, or enters 
+    the word 'quit'. The function returns menu option 'd'. The database menu 
+    is the only menu from which the user can access a list of the sensors. 
+    Therefore, the app must redirect the user to the database menu.
+    
+    Args:
+        engine (sqlalchemy.engine.base.Engine): a SQLAlchemy engine object.
+
+    Returns:
+        str: menu option (always 'd').
+    """
+    continue_asking = True
+    prompt_msg_serial_nr = "  Enter serial number"
+    prompt_msg_type = "  Enter type"
+    console = Console()
+    console.print(Panel("Add Sensor"))
+    while continue_asking:
+        sensor_serial_number = Prompt.ask(prompt_msg_serial_nr)
+        sensor_type = Prompt.ask(prompt_msg_type)
+        if sensor_serial_number == "quit" or sensor_type == "quit":
+            continue_asking = False
+        else:
+            continue_asking = (
+                False if add_sensor(
+                    engine, sensor_serial_number, sensor_type
+                ) else True
+            )
+    return "d"
