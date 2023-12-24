595ba5cf2d1f3ff90b3e81a6ca4aa3885d49799d
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-24 10:25:49 +0100

    Add model for sensor locations table

--- src/humipy/database/models.py
+++ src/humipy/database/models.py
@@ -20,3 +20,14 @@ sensors_table = Table(
     Column("sensor_serial_number", String, nullable=False),
     Column("sensor_type", String, nullable=True),
 )
+
+
+sensor_locations_table = Table(
+    "sensor_locations",
+    metadata,
+    Column("sensor_location_id", Integer, primary_key=True),
+    Column("sensor_id", Integer, nullable=False),
+    Column("location_id", Integer, nullable=False),
+    Column("start_placement", Date, nullable=False),
+    Column("stop_placement", Date, nullable=True),
+)
