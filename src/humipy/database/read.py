638def1cb86d7d2b78bcc5ef103d761ff07d522b
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-25 11:20:40 +0100

    Add getter functions
    This commit adds two getter functions, one
    to retrieve all sensor locations, and a second
    one to retrieve the open sensor locatoins. There
    is also a helper function to construct a
    base query.

--- src/humipy/database/read.py
+++ src/humipy/database/read.py
@@ -34,3 +34,65 @@ def get_sensors(engine: sqlalchemy.engine.base.Engine) -> pd.DataFrame:
     with engine.connect() as conn:
         df = pd.read_sql_query(stmt, conn)
     return df
+
+
+def get_sensor_locations(
+        engine: sqlalchemy.engine.base.Engine) -> pd.DataFrame:
+    """
+    This function retrieves all sensor locations, both open and closed, from 
+    the sensor locations database table.
+
+    Args:
+        engine (sqlalchemy.engine.base.Engine): a SQLAlchemy engine object.
+
+    Returns:
+        pd.DataFrame: a data frame with all sensor locations.
+    """
+    stmt = _get_sensor_locations_base_query()
+    with engine.connect() as conn:
+        df = pd.read_sql_query(stmt, conn)
+    return df
+
+
+def get_open_sensor_locations(
+        engine: sqlalchemy.engine.base.Engine) -> pd.DataFrame:
+    """
+    This function retrieves all open sensor locations from the sensor 
+    locations database table.
+
+    Args:
+        engine (sqlalchemy.engine.base.Engine): a SQLAlchemy engine object.
+
+    Returns:
+        pd.DataFrame: a data frame with all open sensor locations.
+    """
+    stmt = (
+        _get_sensor_locations_base_query()
+        .where(sensor_locations_table.c.stop_placement == None)
+    )
+    with engine.connect() as conn:
+        df = pd.read_sql_query(stmt, conn)
+    return df
+
+
+def _get_sensor_locations_base_query() -> sqlalchemy.sql.selectable.Select:
+    """
+    This function creates a base query to retrieve all sensor locations, both 
+    open and closed, from the sensor locations database table.
+
+    Returns:
+        sqlalchemy.sql.selectable.Select: a SQLAlchemy select construct.
+    """
+    return (
+        select(sensor_locations_table, sensors_table, locations_table)
+        .join_from(
+            sensor_locations_table,
+            sensors_table,
+            sensor_locations_table.c.sensor_id == sensors_table.c.sensor_id,
+        )
+        .join_from(
+            sensor_locations_table,
+            locations_table,
+            sensor_locations_table.c.location_id == locations_table.c.location_id
+        )
+    )
