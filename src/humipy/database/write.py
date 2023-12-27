bf7d8e26d511c6c0966d57a660b70ff9a6572b00
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-27 20:03:27 +0100

    Define a function to stop placement

--- src/humipy/database/write.py
+++ src/humipy/database/write.py
@@ -115,3 +115,66 @@ def start_sensor_placement(
             }
         )
         conn.commit()
+
+
+def stop_sensor_placement(
+        engine: sqlalchemy.engine.base.Engine,
+        location_name: str,
+        sensor_serial_number: str,
+        stop_placement: Optional[datetime] = None):
+    """
+    This function stops a sensor placement.
+
+    Args:
+        engine (sqlalchemy.engine.base.Engine): a SQLAlchemy engine object.
+        location_name (str): the location name.
+        sensor_serial_number (str): the sensor serial number.
+        stop_placement (Optional[datetime], optional): the stop date. If the 
+            user does not specify a stop date, the current date and time is 
+            entered as the stop date. Defaults to None.
+    """
+    # Initialize a stop placement date if required (i.e., if the user did not 
+    # specify a stop date)
+    stop_placement = (
+        datetime.now() if stop_placement is None else stop_placement
+    )
+
+    # Initialize the required query constructs
+    scalar_sensor_subquery = (
+        select(sensors_table.c.sensor_id)
+        .where(sensors_table.c.sensor_serial_number == bindparam("sensor_serial_number"))
+        .scalar_subquery()
+    )
+    scalar_location_subquery = (
+        select(locations_table.c.location_id)
+        .where(locations_table.c.location_name == bindparam("location_name"))
+        .scalar_subquery()
+    )
+    stmt = (
+        select(sensor_locations_table)
+        .where(
+            sensor_locations_table.c.sensor_id == scalar_sensor_subquery,
+            sensor_locations_table.c.location_id == scalar_location_subquery,
+        )
+    )
+
+    with engine.connect() as conn:
+        res = conn.scalars(
+            stmt,
+            {
+                "sensor_serial_number": sensor_serial_number,
+                "location_name": location_name,
+            }
+        )
+        sensor_location_id = res.first()
+
+    # Initialize the required query constructs
+    stmt = (
+        update(sensor_locations_table)
+        .where(sensor_locations_table.c.sensor_location_id == sensor_location_id)
+        .values(stop_placement=stop_placement)
+    )
+
+    with engine.connect() as conn:
+        conn.execute(stmt)
+        conn.commit()
