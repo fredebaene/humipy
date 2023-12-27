fce326d32ffc86e5925b787192f71e14127f753d
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-27 19:50:26 +0100

    Add imports

--- src/humipy/cli.py
+++ src/humipy/cli.py
@@ -3,8 +3,9 @@ from rich.console import Group
 from rich.panel import Panel
 from humipy.database import connect, read, write
 from humipy.views.locations import render_locations_table, render_location_addition
-from humipy.views.sensors import render_sensors_table
+from humipy.views.sensors import render_sensors_table, render_sensor_addition
 from humipy.views.menus import render_main_menu, render_database_menu
+from humipy.views.sensor_locations import render_open_sensor_locations_table
 from humipy.views.exit import render_app_exit
 
 
