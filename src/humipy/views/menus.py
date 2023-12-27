63cf74d2e5a1eb37756b0629da74bda87f544d0b
Frederick De Baene <frederick.debaene@hotmail.com>
2023-12-27 21:38:25 +0100

    Add main menu option

--- src/humipy/views/menus.py
+++ src/humipy/views/menus.py
@@ -13,8 +13,9 @@ def render_main_menu() -> str:
     console.print(Panel("Main Menu"))
     console.print(Padding("- Manage database \[d]", (0, 0, 0, 2)))
     console.print(Padding("- Quit \[q]", (0, 0, 0, 2)))
+    console.print(Padding("- Show measurements \[e]", (0, 0, 0, 2)))
     console.print("")
-    return Prompt.ask("  What do you want to do?", choices=["d", "q"])
+    return Prompt.ask("  What do you want to do?", choices=["d", "e", "q"])
 
 
 def render_database_menu() -> str:
