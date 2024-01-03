8acfb3e6fdc9dfe5c77e31406200d6289de4f59e
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-03 22:18:49 +0100

    Adjust get connection

--- src/humipy/database/connect.py
+++ src/humipy/database/connect.py
@@ -14,14 +14,13 @@ def get_engine(dev: bool = False) -> sqlalchemy.engine.base.Engine:
     """
     if dev:
         return create_engine("sqlite+pysqlite:///:memory:")
+    
     load_dotenv()
-
     db = getenv("DB_NAME")
     host = getenv("DB_HOST")
     port = getenv("DB_PORT")
     username = getenv("DB_USERNAME")
     password = getenv("DB_PASSWORD")
-
     return create_engine(
         f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db}"
     )
