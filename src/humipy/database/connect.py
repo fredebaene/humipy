08eb17b4d9e6aecb1b7f191c174d15ef0bfc265a
Frederick De Baene <frederick.debaene@hotmail.com>
2024-01-03 22:28:44 +0100

    Create test database

--- src/humipy/database/connect.py
+++ src/humipy/database/connect.py
@@ -1,4 +1,8 @@
 from dotenv import load_dotenv
+from humipy.database.models import metadata
+from humipy.database.write import (
+    add_location, add_sensor, start_sensor_placement
+)
 from os import getenv
 import sqlalchemy
 from sqlalchemy import create_engine
@@ -13,7 +17,9 @@ def get_engine(dev: bool = False) -> sqlalchemy.engine.base.Engine:
         sqlalchemy.engine.base.Engine: a SQLAlchemy engine object.
     """
     if dev:
-        return create_engine("sqlite+pysqlite:///:memory:")
+        engine = create_engine("sqlite+pysqlite:///:memory:")
+        _create_test_database(engine)
+        return engine
     
     load_dotenv()
     db = getenv("DB_NAME")
@@ -24,3 +30,20 @@ def get_engine(dev: bool = False) -> sqlalchemy.engine.base.Engine:
     return create_engine(
         f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{db}"
     )
+
+
+def _create_test_database(engine: sqlalchemy.engine.base.Engine) -> None:
+    """
+    This function create a test database. The test database is populated with 
+    dummy data. The test database enables the user to interacht with the 
+    application
+
+    Args:
+        engine (sqlalchemy.engine.base.Engine): a SQLAlchemy engine object.
+    """
+    metadata.create_all(engine)
+    for location in ["Kitchen", "Bathroom", "Bedroom"]:
+        add_location(engine, location)
+    sensors = [("XIE-385A92H20-T0", "DHT11"), ("XIA-502A92V37-T7", "DHT11")]
+    for sensor in sensors:
+        add_sensor(engine, sensor[0], sensor[1])
